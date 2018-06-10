d117afccad01ae3d1791f2baf4b18eb2ae03dcd8
Azmarie <azmarie.yx@gmail.com>
2018-06-10 11:53:46 -0700

    Remove .DS_Store from everywhere

--- server.js
+++ server.js
@@ -7,3 +7,7 @@ var options = {
 var port = process.env.PORT || 9999;
 app.use('/', express.static(__dirname + '/assets', options));
 app.listen(port, function() { console.log('meow listening')});
+
+app.use(function (req, res, next) {
+  res.status(404).sendFile(__dirname + '/assets/404/404.html')
+})
