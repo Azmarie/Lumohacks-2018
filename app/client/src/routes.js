ea73a4286896c20c8ce63ecdcb4caa1a35c5ed82
jnwu <nan.wu.eng@gmail.com>
2018-06-19 23:30:17 -0700

    [lumohack] Updated 404 page

--- app/client/src/routes.js
+++ app/client/src/routes.js
@@ -213,7 +213,7 @@ angular.module('reg')
       })
       .state('404', {
         url: "/404",
-        templateUrl: "views/404.html",
+        templateUrl: "views/404/404.html",
         data: {
           requireLogin: false
         }
